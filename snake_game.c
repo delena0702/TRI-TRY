b41840a54f69db5090058222c49b4c9447b6f385
trevel1959 <55749794+trevel1959@users.noreply.github.com>
2019-11-17 02:18:50 +0900

    개발중 191117
    게임 내부 개발중

--- snake_game.c
+++ snake_game.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 #include <signal.h>
 #include <sys/time.h>
+#include <sys/ioctl.h>
 #include <string.h>
-#include <stdlib.h>
 #include <errno.h>
 
 //정의; 맵의 크기 (토의 후 변경 가능)
@@ -30,18 +29,17 @@ typedef struct Position {
 	int y;
 } Pst;
 
-//타이머가 실행될 때마다 실행되는 함수. 여기서 뱀을 움직일 것.
-void snake_move(int signum){
-	static int count = 0;
-	fprintf(stderr, "test : %d\n", count++);
-}
+Bool IsSnakeMove;	//뱀이 시간마다 움직여야 하는데 타이머핸들러 함수에서는 변수를 넘겨받을 수 없으므로 플래그값만 전역변수로 만들어준다.
+
+void timer_handler(int signum);	//타이머가 실행될 때마다 실행되는 함수. 여기서 뱀을 움직이는 플래그를 TRUE로 바꾼다.
+void object_print(int ch);
 
 void snakeGame(int fd[])
 {	
 	//다음의 구문은 단위 시간마다 Signal을 발생시켜, 뱀을 규칙적으로 움직이게 하기 위한 구조체들을 정의하기 위한 구문이다.
 	///SIGALRM 처리를 위한 구조체.
 	struct sigaction sa;
-	sa.sa_handler = &snake_move;
+	sa.sa_handler = &timer_handler;
 	sigaction(SIGALRM, &sa, NULL);
 	
 	///SIGALAM의 interval을 정의하기 위한 구조체.
@@ -52,31 +50,71 @@ void snakeGame(int fd[])
 	timer.it_interval.tv_usec = 250000;	//한 번 실행 이후에 대한 interval
 	setitimer (ITIMER_REAL, &timer, NULL);	//위에서 정의한 시간마다 SIGALRM을 발생시킨다.
 
+	//다음의 구문은 스네이크 게임을 다른 게임과 함께 출력하기 위해 위치를 조정하고자, 창 크기를 받아오는 구문이다.
+	//터미널의 크기를 반각 글자 단위로 받아온다.
+	struct winsize w;
+   	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+	
+	///화면 크기를 출력하는 테스트용 코드.
+   	printf ("lines %d\n", w.ws_row);
+    printf ("columns %d\n", w.ws_col);
+
 	//다음의 구문은 게임에서 사용할 변수들을 정의하기 위한 구문이다.
+	int x, y;
+
 	///0. 맵
-	int map[MAP_SIZE + 2][MAP_SIZE + 2] = { EMPTY, };	//맵을 표현하기 위한 2차원 배열. 벽 때문에 +2
-	int x;
+	int map[MAP_SIZE + 2][MAP_SIZE + 2];	//맵을 표현하기 위한 2차원 배열. 벽 때문에 +2
+	for(y = 0; y < MAP_SIZE; y++){
+		for(x = 0; x < MAP_SIZE; x++){
+			map[y][x] = EMPTY;
+		}
+	}
 	for (x = 0; x < MAP_SIZE - 1; x++) {
 		map[0][x] = map[x][MAP_SIZE - 1] = map[MAP_SIZE - 1][MAP_SIZE - 1 - x] = map[MAP_SIZE - 1 - x][0] = WALL;	//맵에 벽을 만들어준다.
 	}
 
 	///1. 뱀
 	Pst snake[MAP_SIZE * MAP_SIZE] = { {0,0}, };	//뱀의 머리와 각 몸통의 위치를 저장할 배열.
+	int snakeLength = 0;
+	int snakeLengthMAX = 3;	//처음에는 3칸으로 시작한다.
 	int snakeDirection = EAST;
 	int snakeDirection_OLD = EAST;
 
 	///2. 사과
 	Pst apple = { 0,0 };	//사과의 위치를 저장할 변수.
+	Bool IsAppleExist;	//사과 존재 여부.
 	
 	char ch;	//입력 키를 저장하기 위한 변수.
 
-	while (TRUE)
-	{
+	//다음 구문은 게임을 초기화한다.
+	srand((int)time(NULL));
+
+	IsSnakeMove = FALSE;
+	snake[0].x = snake[0].y = MAP_SIZE / 2;
+	map[snake[0].y][snake[0].x] = SNAKE;
+	snakeLength++;
+
+	IsAppleExist = TRUE;
+	apple.x = (rand() % MAP_SIZE) + 1;
+	apple.y = (rand() % MAP_SIZE) + 1;
+	map[apple.y][apple.x] = APPLE;
+	
+	//맵 출력
+	for(y = 0; y < MAP_SIZE; y++){
+		for(x = 0; x < MAP_SIZE; x++){
+			gotoxy(w.ws_col - 2 - MAP_SIZE*2 + 2*x, y + 1);
+			//fprintf(stderr, "%d ", map[y][x]);
+			object_print(map[y][x]);
+		}
+		fprintf(stderr, "\n");
+	}
+
+	while (TRUE) {
 		int readByte;
 		ch = 0;
 		readByte = read(fd[0], &ch, 1);	//부모 프로세스에서 pipe를 통해 키보드 값을 읽어옴
 
-		if(readByte == 1){	//값을 읽어온 경우에만
+		if(readByte == 1) {	//값을 읽어온 경우에만
 			switch (ch)
 			{
 			case 65:	//위
@@ -100,41 +138,87 @@ void snakeGame(int fd[])
 				break;
 			}
 		}
+		else {	//값을 읽어오지 않았을 경우 (== 인터럽트 당했을 경우)
+			if(IsSnakeMove == TRUE) {
+				//뱀의 머리를 이동시킨다.
+				switch (snakeDirection){
+				case EAST: //머리가 동쪽일 경우
+					snake[snakeLength].x = snake[snakeLength - 1].x + 1;
+					snake[snakeLength].y = snake[snakeLength - 1].y;
+					snakeDirection_OLD = EAST;
+					break;
+				case WEST: //서
+					snake[snakeLength].x = snake[snakeLength - 1].x - 1;
+					snake[snakeLength].y = snake[snakeLength - 1].y;
+					snakeDirection_OLD = WEST;
+					break;
+				case SOUTH: //남
+					snake[snakeLength].x = snake[snakeLength - 1].x;
+					snake[snakeLength].y = snake[snakeLength - 1].y + 1;
+					snakeDirection_OLD = SOUTH;
+					break;
+				case NORTH: //북
+					snake[snakeLength].x = snake[snakeLength - 1].x;
+					snake[snakeLength].y = snake[snakeLength - 1].y - 1;
+					snakeDirection_OLD = NORTH;
+					break;
+				}
+
+				//이동한 뱀의 머리가 맵에서는 어느 위치인지 파악한다.
+				switch(map[snake[snakeLength].y][snake[snakeLength].x]){
+				case WALL:
+					return;	//게임 끝		
+
+				case APPLE:
+					snakeLengthMAX--;	//최대 뱀 길이를 감소시킨다.
+					IsAppleExist = FALSE;
+					//break를 씌우지 않고 넘김으로써 EMPTY와 같이 처리되도록 한다. (SNAKE 구문은 어차피 통과할 것이므로.)
+
+				case SNAKE:
+					if (!((snake[0].x == snake[snakeLength].x) && (snake[0].y == snake[snakeLength].y)))
+						return;	//머리 위치와 꼬리 위치가 아니라면 게임 끝. 꼬리는 바로 움직여서 꼬리 위치는 빈 칸이 될 것이므로 종료시키지 않아도 된다.
+					//break를 씌우지 않고 넘김으로써 EMPTY와 같이 처리되도록 한다.
+
+				case EMPTY:
+					snakeLength++;
+					if(snakeLength > snakeLengthMAX){
+
+					}
+					break;
+				}
+				
+				IsSnakeMove = FALSE;
+			}
+		}
+	}
+}
+
+void timer_handler(int signum){
+	IsSnakeMove = TRUE;
+}
+
+void object_print(int ch) {
+	switch (ch) {
+	case EMPTY:
+		fprintf(stderr, "☆");
+		break;
+	case APPLE:
+		fprintf(stderr, "★");
+		break;
+	case WALL:
+		fprintf(stderr, "▨");
+		break;
+	case SNAKE:
+		fprintf(stderr, "■");
+		break;
 	}
 }
 
 /*
 
 int main() {
-	int Snake_length, MAX_Snake_length, Snake_direction, Snake_direction_O, Snake_speed;	//�� ����, �� �ִ� ����, �� ����, �� ���� ����, ���� �ӵ�
-	int Apple_x, Apple_y;																	//��� ��ǥ
-	BOOL Is_Apple_exist;																	//����� ���� ����
-	BOOL Is_Game_move, Is_Pause_stop;														//������ ���� ����, ������ �Ͻ����� ����, �Ͻ����� �����
 
 	while (Is_Game) { //������ ��� �Ѵٰ� �ϸ�
-
-		srand((int)time(NULL));			//rand�Լ� �ʱ�ȭ
-
-		Start_time = Timer = Time_O = Snake_length = Snake_speed = 0;
-		MAX_Snake_length = 3;
-		Is_Apple_exist = Is_Pause_stop = FALSE;				//�� �� �ʱ�ȭ
-		Snake_direction = East;							//�� �Ӹ��� �������� ������
-
-		system("cls");					//ȭ�� ����
-		Map_print();					//�ٽ� �־���
-
-		Is_Ingame = Is_Game_move = TRUE;	//���� �����Ѵ�!!!!
-
-		system("cls");					//���̵� â ����
-		Map_print();					//�ٽ� �̻ڰ� ����Ʈ
-
-		Start_time = clock();			//���۽ð�
-		Snake_x[0] = Snake_y[0] = 9;	//���� ��ġ
-		gotoxy(18, 9);
-		Text_color(0x002F);				//�� �Ӹ� ���
-		Snake_print(0);
-		Text_color(0x0007);
-		Snake_length++;					//�Ӹ� �������� ���� 1��
 
 		while (Is_Ingame) {
 			Timer = (int)((clock() - Start_time) / Snake_speed); //���� �ð��� ���
@@ -142,48 +226,7 @@ int main() {
 			//여기부터 단위시간마다 뱀이 움직이는 부분인거 같음
 			if (Timer != Time_O && Is_Game_move) { //�ð��� ������
 
-				switch (Snake_direction) {
-				case East: //���� �������� ��ǥ�� ������ (���۽� �⺻���� ����������)
-					Snake_x[Snake_length] = Snake_x[Snake_length - 1] + 1;
-					Snake_y[Snake_length] = Snake_y[Snake_length - 1];
-					Snake_direction_O = East;
-					break;
-				case West: //��
-					Snake_x[Snake_length] = Snake_x[Snake_length - 1] - 1;
-					Snake_y[Snake_length] = Snake_y[Snake_length - 1];
-					Snake_direction_O = West;
-					break;
-				case South: //��
-					Snake_x[Snake_length] = Snake_x[Snake_length - 1];
-					Snake_y[Snake_length] = Snake_y[Snake_length - 1] + 1;
-					Snake_direction_O = South;
-					break;
-				case North: //��
-					Snake_x[Snake_length] = Snake_x[Snake_length - 1];
-					Snake_y[Snake_length] = Snake_y[Snake_length - 1] - 1;
-					Snake_direction_O = North;
-					break;
-				}
-
 				switch (Map[Snake_y[Snake_length]][Snake_x[Snake_length]]) { // �Ӹ��� �Ű��� ĭ��
-				case Wall:										//���̸�
-					if (!Is_Mode_Debug) Is_Ingame = FALSE;				//����� ��尡 �ƴ� �� ���� ��
-					break;
-
-				case Apple:										//�����
-					MAX_Snake_length++;								//�Ѱ� ���̸� �÷��ְ�
-					if (MAX_Snake_length == 324) Is_Ingame = FALSE;
-					Is_Apple_exist = 0;								//����� ���ٰ� �˷��ش�
-																	//break�� ���� ������ ��� �԰� �ؿ� �Լ� �����϶��
-
-				case Snake:										//���̸�
-					if (Is_Apple_exist) {							//����� �����ϸ� == case Apple�� ������ �ʾ�����
-						if (!Is_Mode_Debug) {						//����� ��尡 �ƴѵ�
-							if (!((Snake_x[0] == Snake_x[Snake_length]) && (Snake_y[0] == Snake_y[Snake_length]))) Is_Ingame = FALSE; break;	//������ �ƴϸ� ���� ��
-						}
-						else if (!Is_Mode_Noclip) break;			//��Ŭ�� ���� �� ���� �������
-					}
-
 				case 0:											//�� �����̸�
 					Snake_length++;									//�� ����++
 					if (Snake_length > MAX_Snake_length) {			//�� ���̰� �ִ븦 ������
@@ -197,6 +240,7 @@ int main() {
 						Snake_x[MAX_Snake_length] = Snake_y[MAX_Snake_length] = 0; //�׷� ������(m��°�� �ϸ�)�̶� m-1�̶� ��ġ�ϱ� �������� ������
 						Snake_length--;								 //�ִ� ���̷� �����ش�
 					}
+
 					Text_color(0x0027);										//������ �ʷϹ�濡 �����
 					for (x = 0; x < Snake_length - 1; x++) Snake_print(x);	//�� ��翡 �°� �ٽ� ����Ʈ ���ش�
 					Text_color(0x002F);										//���� �ʷϹ�濡 ���� ��� ��
@@ -216,43 +260,8 @@ int main() {
 				Object_move(Apple_x, Apple_y, Apple);				//��� ����Ʈ ���� ��
 				Is_Apple_exist = TRUE;								//��� �ڿ� ��� �ִٰ� ���ش�
 			}
-
-			//여기부터 키보드 입력에 관한 부분인 듯.
-
-			if (_kbhit()) {						//Ű���� �Է��� ������
-				key = _getch();						//Ű�� �ް�
-
-				switch (key) {						//�ݴ�������� ���°� �ƴϸ� Ű�� ���� ������ �ٲ��ش�
-				case 77: if (Snake_direction_O != West && Is_Game_move) Snake_direction = East; break;
-				case 75: if (Snake_direction_O != East && Is_Game_move) Snake_direction = West; break;
-				case 80: if (Snake_direction_O != North && Is_Game_move) Snake_direction = South; break;
-				case 72: if (Snake_direction_O != South && Is_Game_move) Snake_direction = North; break;
-				}
-			}
 		}
 	}
-}
-
-//�� �ʱ�ȭ �ϴ� �Լ�
-void Snake_clear(int n) {
-	for (x = 0; x < n; x++) {
-		Map[Snake_y[x]][Snake_x[x]] = 0;	//���� �ʱ�ȭ��
-		Snake_x[x] = Snake_y[x] = 0;		//�� �迭�� �ʱ�ȭ��
-	}
-	Snake_x[n] = Snake_y[n] = 0;			//�� ������ �쵵 �ʱ�ȭ����
-}
-
-//�� ����Ʈ ���ִ� �Լ�
-void Map_print() {
-	for (x = 0; x < 20; x++) {
-		for (y = 0; y < 20; y++) {
-			//Text_color((x+y)%16)
-			gotoxy(2 * x, y);
-			Object_print(Map[y][x]);
-		}
-	}
-	//Text_color(0x0007);
-	Debug_Show();
 }
 
 //�� ĭ�� ������Ʈ�� �ٲ��ִ� �Լ�
@@ -279,53 +288,5 @@ void Object_print(int a) {
 			puts("��");
 			break;
 	}
-}
-
-//Ư�� ������� ���� ������ִ� �Լ�
-void Snake_print(int n) {
-	int Dx_Before, Dy_Before, Dx_After, Dy_After;	//�� ������ �������� ��/�� ������ ��ǥ ��ȭ
-	BOOL Up, Down, Left, Right;						//�� ������ �������� �����¿쿡 ������ �ִ��� üũ�ϴ� ����
-	Up = Down = Left = Right = FALSE;				//�ϴ� �� ������ �����¿쿡 ���� �����ϴ�
-	Map[Snake_y[n]][Snake_x[n]] = Snake;			//�׸��� �ϴ� �� ��ǥ���� ���� �ֽ��ϴ�
-
-	if (n != 0) {									//�̰� ������ �ƴ϶��
-		Dx_Before = Snake_x[n] - Snake_x[n - 1];		// Dx = 1; ���� ĭ�� ����, Dx = -1; ���� ĭ�� ������
-		Dy_Before = Snake_y[n] - Snake_y[n - 1];		// Dy = 1; ���� ĭ�� �Ʒ���, Dy = -1; ���� ĭ�� ����
-	}
-	else Dx_Before = Dy_Before = 0;					//������ ���� ���� ���ٰ� �˷���
-
-	if (Snake_x[n + 1] != 0) {						//�̰� �Ӹ��� �ƴ϶�� (�Ӹ� ���� ���İ��� ��ǥ���� 0���� ���������Ƿ�)
-		Dx_After = Snake_x[n] - Snake_x[n + 1];			// Dx = 1; ���� ĭ�� ����, Dx = -1; ���� ĭ�� ������
-		Dy_After = Snake_y[n] - Snake_y[n + 1];			// Dy = 1; ���� ĭ�� �Ʒ���, Dy = -1; ���� ĭ�� ����
-	}
-	else Dx_After = Dy_After = 0;					//�Ӹ��� ���� ���� ���ٰ� �˷���
-
-	if (Dx_Before == 1 || Dx_After == 1)	Left = TRUE;	//���ʿ� ���� �ֳ�??
-	if (Dx_Before == -1 || Dx_After == -1)	Right = TRUE;	//������
-	if (Dy_Before == 1 || Dy_After == 1)	Up = TRUE;		//����
-	if (Dy_Before == -1 || Dy_After == -1)	Down = TRUE;	//�Ʒ���
-
-	gotoxy(2 * Snake_x[n], Snake_y[n]);				//�� ĭ���� ����
-
-	//���� �������� ���ڴ� Ű�е� ������ (ex (24) = ��, �¿� ���� ����, (20) == �󿡸� ���� ����)
-	if (Up) {							//���� ���� ����
-		if (Left) puts("��");				//���ʿ� ���� ����						(24)
-		else if (Right) puts("��");			//�����ʿ� ���� ����					(26)
-		else puts("��");					//�Ʒ��� ���� �ִ� ���� �������		(20, 28)
-	}
-
-	else if (Left) {					//���ʿ� ���� ����
-		if (Down) puts("��");				//�Ʒ��ʿ� ���� ����					(48)
-		else puts("��");					//�����ʿ� ���� �ִ� ���� �������		(40, 46)
-	}
-
-	else if (Right) {					//�����ʿ� ���� ����
-		if (Down) puts("��");				//�Ʒ��ʿ� ���� ����					(68)
-		else printf("��");					//�̹� ���¿� ���� �����Ƿ�				(60)
-	}
-
-	else if(Down) printf("��");			//�̹� �����¿� ���� �����Ƿ�				(80)
-
-	else printf("��");					//�� ó�� ������ ������ ���ϹǷ�			(00)
 }
 */
